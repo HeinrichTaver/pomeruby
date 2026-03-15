4106d9346e5468a5a3d66201fbe5f0cd440494b2
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2026-03-15 17:44:34 -0300

    feat: add a very limited timer

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
 gem "bubbletea", "~> 0.1.3"
+
+gem "bubbles", "~> 0.1.1"
--- pomeruby.rb
+++ pomeruby.rb
@@ -1,3 +1,4 @@
+require 'bubbles'
 require 'bubbletea'
 require 'lipgloss'
 
@@ -5,16 +6,21 @@ class Pomeruby
   include Bubbletea::Model
 
   def initialize
-    @title_style =
+    @timer = Bubbles::Timer.new(60 * 25)
+
+    @text_bold =
       Lipgloss::Style.new
         .bold(true)
-    @help_style =
+    @text_italic =
       Lipgloss::Style.new
         .italic(true)
   end
 
   def init
-    [self, Bubbletea.set_window_title('Pomeruby')]
+    [self, lambda {
+      Bubbletea.set_window_title('Pomeruby')
+      @timer.init
+     }]
   end
 
   def update(message)
@@ -24,18 +30,34 @@ class Pomeruby
       when 'q', 'ctrl+c'
         [self, Bubbletea.quit]
       end
+    when Bubbles::Timer::TickMessage, Bubbles::Timer::StartStopMessage
+      @timer, command = @timer.update(message)
+
+      [self, command]
     else
       [self, nil]
     end
   end
 
   def view
+    timer = if @timer.timed_out?
+              @text_bold.render("Block's up. Pause, now.")
+            else
+              @timer.view
+            end
+
     lines = []
-    lines << Lipgloss.place(80, 0, :center, :center, @title_style.render('Pomeruby'))
+    lines << Lipgloss.place(80, 0,
+                            :center, :center,
+                            @text_bold.render('Pomeruby'))
     lines << ''
-    lines << Lipgloss.place(80, 10, :center, :center, 'Hello, World!')
+    lines << Lipgloss.place(80, 10,
+                            :center, :center,
+                            timer)
     lines << ''
-    lines << Lipgloss.place(80, 0, :center, :center, @help_style.render('Press q to quit'))
+    lines << Lipgloss.place(80, 0,
+                            :center, :center,
+                            @text_italic.render('Press q to quit'))
     lines.join("\n")
   end
 end
