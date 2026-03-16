d78984ed9de55fdc3697a08d632de53a9593980b
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2026-03-15 23:20:04 -0300

    feat: add start and toggle features for timer

--- pomeruby.rb
+++ pomeruby.rb
@@ -3,10 +3,16 @@ require 'bubbletea'
 require 'lipgloss'
 
 class Pomeruby
+  POMO_BLOCK_IN_MINUTES      = 60 * 25
+  POMO_PAUSE_IN_MINUTES      = 60 * 5
+  POMO_PAUSE_LONG_IN_MINUTES = 60 * 30
+
   include Bubbletea::Model
 
   def initialize
-    @timer = Bubbles::Timer.new(60 * 25)
+    @timer         = Bubbles::Timer.new(POMO_BLOCK_IN_MINUTES)
+    @timer_started = false
+    @timer_paused  = false
 
     @text_bold =
       Lipgloss::Style.new
@@ -17,10 +23,7 @@ class Pomeruby
   end
 
   def init
-    [self, lambda {
-      Bubbletea.set_window_title('Pomeruby')
-      @timer.init
-     }]
+    [self, Bubbletea.set_window_title('Pomeruby')]
   end
 
   def update(message)
@@ -29,36 +32,71 @@ class Pomeruby
       case message.to_s
       when 'q', 'ctrl+c'
         [self, Bubbletea.quit]
+      when 's'
+        if !@timer.running? && !@timer_started
+          @timer         = Bubbles::Timer.new(POMO_BLOCK_IN_MINUTES)
+          @timer_started = true
+
+          [self, @timer.init]
+        end
+      when ' ', 'space'
+        if @timer_started && @timer.running?
+          @timer_paused = true
+        else
+          @timer_paused = false
+        end
+
+        [self, @timer.toggle]
       end
     when Bubbles::Timer::TickMessage, Bubbles::Timer::StartStopMessage
-      @timer, command = @timer.update(message)
+      if @timer_started
+        @timer, command = @timer.update(message)
 
-      [self, command]
+        [self, command]
+      else
+        [self, nil]
+      end
     else
       [self, nil]
     end
   end
 
   def view
-    timer = if @timer.timed_out?
-              @text_bold.render("Block's up. Pause, now.")
-            else
-              @timer.view
-            end
+    timer =
+      if @timer.timed_out?
+        @timer_started = false
+        @text_bold.render("Block's up. Pause, now.")
+      elsif @timer_started
+        @timer.view
+      else
+        'Hello, World!'
+      end
 
     lines = []
-    lines << Lipgloss.place(80, 0,
-                            :center, :center,
-                            @text_bold.render('Pomeruby'))
+    lines << place_centered(80, 0, @text_bold.render('Pomeruby'))
     lines << ''
-    lines << Lipgloss.place(80, 10,
-                            :center, :center,
-                            timer)
     lines << ''
-    lines << Lipgloss.place(80, 0,
-                            :center, :center,
-                            @text_italic.render('Press q to quit'))
+    lines << place_centered(80, 0, timer)
+    lines <<
+      if @timer_paused
+        place_centered(80, 0, '(paused)')
+      else
+        place_centered(80, 0, '')
+      end
+    lines << ''
+    lines <<
+      if @timer_started
+        place_centered(80, 0, @text_italic.render('Press space to toggle'))
+      else
+        place_centered(80, 0, @text_italic.render('Press s to start'))
+      end
+    lines << place_centered(80, 0, @text_italic.render('Press q to quit'))
     lines.join("\n")
+  end
+
+  private
+  def place_centered(width, height, text)
+    Lipgloss.place(width, height, :center, :center, text)
   end
 end
 
